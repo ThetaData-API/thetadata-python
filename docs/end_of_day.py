1c3f4eef41017bb34507ff528037aade1782ee5a
Adler Weber <adlerweber8430@gmail.com>
2022-07-29 19:22:20 -0400

    Add end of day test and snippet

---
+++ docs/end_of_day.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from datetime import date
+from thetadata import ThetaClient, OptionReqType, OptionRight, DateRange
+
+
+def end_of_day() -> pd.DataFrame:
+    """Request end-of-day data between 7/18/2022 and 7/22/2022."""
+    # Create a ThetaClient
+    client = ThetaClient()
+
+    # Connect to the Terminal
+    with client.connect():
+
+        # Make the request
+        response = client.get_hist_option(
+            req=OptionReqType.EOD,
+            root="AAPL",
+            exp=date(2022, 8, 12),
+            strike=140,
+            right=OptionRight.CALL,
+            date_range=DateRange(date(2022, 7, 18), date(2022, 7, 22)),
+        )
+
+    return response
+
+
+if __name__ == "__main__":
+    response = end_of_day()
+    print(response)
