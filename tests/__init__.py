db954e53849f469e8ca5cf0bd39554192accc06d
Adler Weber <adlerweber8430@gmail.com>
2022-07-28 20:34:52 -0400

    Add Terminal kill method

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
 """Package containing tests for the ThetaData Python API."""
+import pytest
+from thetadata import ThetaClient
+
+
+@pytest.fixture
+def tc():
+    """Generate a ThetaClient connected to the Terminal."""
+    client = ThetaClient(timeout=15)
+    with client.connect():
+        yield client
