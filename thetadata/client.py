422937e4c80e4348346c73182b1e72a83432d020
Baile <baileydan911@gmail.com>
2023-03-09 12:35:24 -0500

    - Handles missing SSL certs for the terminal download - Added MS_OF_DAY2 enum

--- thetadata/client.py
+++ thetadata/client.py
@@ -23,7 +23,7 @@ from .parsing import (
 from .terminal import check_download, launch_terminal
 
 _NOT_CONNECTED_MSG = "You must establish a connection first."
-_VERSION = '0.8.7'
+_VERSION = '0.8.8'
 
 
 def _format_strike(strike: float) -> int:
